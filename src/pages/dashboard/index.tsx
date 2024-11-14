f5cfd8c109640b5ceca307cc46c94168df6a9b93
Chandula Samarasinghe <Chandulas1999@gmail.com>
2024-11-14 20:06:40 +0530

    Add FinancialRecordForm and FinancialRecordList components to Dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,13 @@
 import { useUser } from "@clerk/clerk-react";
+import { FinancialRecordForm } from "./financial-record-form";
+import { FinacialRecordList } from "./financial-record-list";
 export const Dashboard = () => {
   const { user } = useUser();
   return (
     <div className="dashboard-contaner">
       <h1>Welcome {user?.firstName}! Here Are Your Finance</h1>
+      <FinancialRecordForm />
+      <FinacialRecordList />
     </div>
   );
 };
