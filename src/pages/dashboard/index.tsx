eb0fa8af107b2b44e09c29bea3bfc5a34e49bccb
Chandula Samarasinghe <Chandulas1999@gmail.com>
2025-01-07 18:57:16 +0530

    Add total monthly calculation to dashboard and fix component import

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,13 +1,28 @@
 import { useUser } from "@clerk/clerk-react";
 import { FinancialRecordForm } from "./financial-record-form";
-import { FinacialRecordList } from "./financial-record-list";
+import { FinancialRecordList } from "./financial-record-list";
+import "./financial-record.css";
+import { useFinancialRecords } from "../../contexts/financial-record-context";
+import { useMemo } from "react";
 export const Dashboard = () => {
   const { user } = useUser();
+  const { records } = useFinancialRecords();
+
+  const totalMonthly = useMemo(() => {
+    let totalAmount = 0;
+    records.forEach((record) => {
+      totalAmount += record.amount;
+    });
+
+    return totalAmount;
+  }, [records]);
+
   return (
-    <div className="dashboard-contaner">
-      <h1>Welcome {user?.firstName}! Here Are Your Finance</h1>
+    <div className="dashboard-container">
+      <h1> Welcome {user?.firstName}! Here Are Your Finances:</h1>
       <FinancialRecordForm />
-      <FinacialRecordList />
+      <div>Total Monthly: ${totalMonthly}</div>
+      <FinancialRecordList />
     </div>
   );
 };
